29976f0fd4985924a0a0a1f5c8fbb9139551d56e
Naveen Sagar <sagarnaveen1995@gmail.com>
2017-10-21 15:52:02 +0530

    Initialize the RecyclerView project

---
+++ app/src/main/java/com/example/test/recyclerview/ListItem.java
@@ -0,0 +1,90 @@
+package com.example.test.recyclerview;
+
+/**
+ * Created by naveen on 21/10/17.
+ */
+
+public class ListItem {
+    // Default translation of the word
+    private String mDefaultTranslation;
+
+    // Miwok translation of the word
+    private String mMiwokTranslation;
+
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+    /**
+     * Image resource ID for the word
+     */
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+    /**
+     * Audio resource ID for the word
+     */
+    private int mAudioResourceId;
+
+    /**
+     * Create a new Word object.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation is the word in the Miwok language
+     *
+     * @param audioResourceId is resourceId of the audio file of that words Miwok translation
+     */
+    public ListItem(String defaultTranslation, String miwokTranslation, int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mAudioResourceId = audioResourceId;
+    }
+
+    /**
+     * Create a new Word object.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation is the word in the Miwok language
+     *
+     * @param imageResourceId  is resourceId of the image that we want to display with the translations
+     *
+     * @param audioResourceId is resourceId of the audio file of that words Miwok translation
+     */
+    public ListItem(String defaultTranslation, String miwokTranslation, int imageResourceId, int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
+
+    }
+
+    // Get the default translation of the word
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    // Get the Miwok translation of the word
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+    /**
+     * Return the image resource ID of the word.
+     */
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+    /**
+     * Return the audio resource ID of the word.
+     */
+    public int getAudioResourceId() {
+        return mAudioResourceId;
+    }
+
+    /**
+     * Returns whether or not there is an image for this word.
+     */
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+}
